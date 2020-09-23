6170abc4784c6a14754c0c2c67ffc12a39aee41b
Sebastian Mocanu <diemetal851@gmail.com>
2020-09-24 00:55:31 +0300

    Updates

---
+++ precache-manifest.5034ec76aee8fef73917710815f06dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bc4a5a5bf9097a59ee93c2e8075218",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d90174f8fa665864d1d5",
+    "url": "/react-pomodoro-clock/static/css/main.9437f904.chunk.css"
+  },
+  {
+    "revision": "291d4f8c19cd1a787a3b",
+    "url": "/react-pomodoro-clock/static/js/2.d46e7d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro-clock/static/js/2.d46e7d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90174f8fa665864d1d5",
+    "url": "/react-pomodoro-clock/static/js/main.5e4c4e92.chunk.js"
+  },
+  {
+    "revision": "ace7844110ae1b26eb7a",
+    "url": "/react-pomodoro-clock/static/js/runtime-main.5010b995.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  }
+]);
